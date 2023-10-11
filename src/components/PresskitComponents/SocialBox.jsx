4e52b251f34b0a74340acc1124640a80c0935262
aizhak <jbisaac02@gmail.com>
2023-10-10 21:01:31 -0600

    frontbuild

--- src/components/PresskitComponents/SocialBox.jsx
+++ src/components/PresskitComponents/SocialBox.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './SocialBox.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+
 
 const SocialBox = ({ Icon, Red, Nombre, Apodo }) => {
   return (
